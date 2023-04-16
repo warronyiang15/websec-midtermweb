5dc3bd48c57c536694db21ee22f32afd8b9f399e
warronyiang15 <warronyiang15@gmail.com>
2023-04-16 22:39:45 +0800

    fix entry

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,16 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { csrfErrorHandler, doubleCsrfProtection } from "./csrf";
 import rootRouter from "./routes";
-// import {prisma } from "./adapters";
+import { prisma } from "./adapters";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const frontendDir = path.join(__dirname, "../../frontend/dist");
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.static(frontendDir));
 //! Limit it to 1MB enough
 app.use(express.json({limit: '1mb'}));
 
@@ -40,11 +44,18 @@ app.use(doubleCsrfProtection);
 app.use(csrfErrorHandler);
 app.use(rootRouter);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.get("*", (req, res) => {
+    if( !req.originalUrl.startsWith("/api")){
+        return res.sendFile(path.join(frontendDir, "index.html"));
+    }
+    return res.status(404).sendFile(path.join(frontendDir, "assets/404.jpeg"));
+})
 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+process.on('exit', async () => {
+    await prisma.$disconnect();
+})
